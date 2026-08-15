85f906e629da31769b654c80a274cbebd8bdf737
Jack Reichelt <jackreichelt@gmail.com>
2026-08-15 14:59:32 +1000

    Updating vite base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://jackreichelt.github.io/fastapi-adventures-frontend/",
   legacy: { inconsistentCjsInterop: true },
 })
