9807a4134f302d8f00769e9bb36f323fd60bfa86
Jack Reichelt <jackreichelt@gmail.com>
2026-08-15 18:57:45 +1000

    Trying github hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { RouterProvider, createHashRouter } from "react-router-dom"
 
 // Import our pages
 import HomePage from "./pages/HomePage.jsx"
@@ -9,7 +9,7 @@ import VotingPage from "./pages/VotingPage.jsx"
 
 //Import our components
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <HomePage />,
