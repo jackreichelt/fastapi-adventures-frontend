fd95a36106e455d8ee35c785db02289fadb4899e
Jack Reichelt <jackreichelt@gmail.com>
2026-08-15 19:16:47 +1000

    Added click on slide bar to navigate

--- src/components/PollBar.jsx
+++ src/components/PollBar.jsx
@@ -1,14 +1,22 @@
+import { useNavigate } from "react-router-dom"
+
 import "./PollBar.css"
 
 function PollBar(props) {
+    const navigate = useNavigate()
+
     const option = props.option
     const votes = props.votes
     const totalVotes = props.totalVotes
     const votesFraction = votes / totalVotes * 100
 
+    const goToSlide = () => {
+        navigate(`/slide/${option}`)
+    }
+
     return (
         <div className="pollBar">
-            <div className="filledPoll" style={{ 'width': `${votesFraction}%` }}>
+            <div className="filledPoll" style={{ 'width': `${votesFraction}%` }} onClick={goToSlide}>
                 <p>
                     {option.name} - {votes}
                 </p>
