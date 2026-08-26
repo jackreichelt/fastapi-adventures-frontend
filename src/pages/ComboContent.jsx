c80afca6c86c8a738e2ef6a94aaf5a7d7b78d759
Jack Reichelt <jackreichelt@gmail.com>
2026-08-26 11:30:20 +1000

    Added (bad) system for images

--- src/pages/ComboContent.jsx
+++ src/pages/ComboContent.jsx
@@ -2,8 +2,17 @@ import "./theme.css"
 
 import "./ComboContent.css"
 
+// TODO: Make this not awful
+import pydantic1 from "../assets/pydantic1.png"
+import pydantic2 from "../assets/pydantic2.png"
 
 function ComboContent({ slide }) {
+    const images = {
+        placeholder: "https://placehold.co/300",
+        pydantic1,
+        pydantic2
+    }
+
     return (
         <div id="comboDiv">
             <h1 className="title">{slide.title}</h1>
@@ -17,7 +26,7 @@ function ComboContent({ slide }) {
                         )
                     })}
                 </ul>
-                <img className="slideImage" src={slide.image} alt={`Image for ${slide.title}`} />
+                <img className="slideImage" src={images[slide.image]} alt={`Image for ${slide.title}`} />
             </div>
         </div>
     )
--- src/pages/ImageContent.jsx
+++ src/pages/ImageContent.jsx
@@ -2,13 +2,22 @@ import "./theme.css"
 
 import "./ImageContent.css"
 
+// TODO: Make this not awful
+import pydantic1 from "../assets/pydantic1.png"
+import pydantic2 from "../assets/pydantic2.png"
 
 function ImageContent({ slide }) {
+    const images = {
+        placeholder: "https://placehold.co/300",
+        pydantic1,
+        pydantic2
+    }
+
     return (
         <div id="comboDiv">
             <h1 className="title">{slide.title}</h1>
             <div id="imageContentPane">
-                <img className="slideImage" src={slide.image} alt={`Image for ${slide.title}`} />
+                <img className="slideImage" src={images[slide.image]} alt={`Image for ${slide.title}`} />
             </div>
         </div>
     )
